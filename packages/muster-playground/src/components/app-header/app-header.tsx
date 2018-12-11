abd7e4d2a4799713c41df92387efcdc3936d9ca9
Laurence Roberts <lsjroberts@outlook.com>
2018-12-11 13:34:21 +0000

    Remove redundant ie11 endsWith polyfill

--- packages/muster-playground/src/components/app-header/app-header.tsx
+++ packages/muster-playground/src/components/app-header/app-header.tsx
@@ -53,13 +53,3 @@ export default ({ className = undefined, title }: AppHeaderProps) => {
     </header>
   );
 };
-
-// Polyfill for IE11
-if (!String.prototype.endsWith) {
-  String.prototype.endsWith = function(search, this_len) {
-    if (this_len === undefined || this_len > this.length) {
-      this_len = this.length;
-    }
-    return this.substring(this_len - search.length, this_len) === search;
-  };
-}
